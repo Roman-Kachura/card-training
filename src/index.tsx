3d62a29da96e5e6e68d162b5498e3b8d0704caa9
Roman Kachura <roma.kachyra@gmail.com>
2023-06-15 17:22:01 +0200

    Hash router -> browser router

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,11 @@ import {store} from "./app/store";
 import { App } from './app/App';
 
 ReactDOM.render(
-        <HashRouter>
+        <BrowserRouter>
             <Provider store={store}>
             <App/>
             </Provider>
-        </HashRouter>,
+        </BrowserRouter>,
     document.getElementById('root')
 );
 
