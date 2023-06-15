1e56044ab3a5782afc56c25300460bb98c914e68
Roman Kachura <roma.kachyra@gmail.com>
2023-06-15 18:16:26 +0200

    browser > hash

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,11 @@ import {store} from './app/store';
 import {App} from './app/App';
 
 ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
+    <HashRouter>
+        <Provider store={store}>
             <App/>
-            </Provider>
-        </BrowserRouter>,
+        </Provider>
+    </HashRouter>,
     document.getElementById('root')
 );
 
